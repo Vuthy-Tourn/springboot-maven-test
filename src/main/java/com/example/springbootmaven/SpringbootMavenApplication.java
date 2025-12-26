83e8aa42c0f148da90f565fb5798a24b7c0a337a
Vuthy_Tourn <vuthytuon168@gmail.com>
2025-12-27 01:52:58 +0700

    Modified: application not initialize JPA

--- src/main/java/com/example/springbootmaven/SpringbootMavenApplication.java
+++ src/main/java/com/example/springbootmaven/SpringbootMavenApplication.java
@@ -2,8 +2,10 @@ package com.example.springbootmaven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class SpringbootMavenApplication {
 
     public static void main(String[] args) {
